6ad4c10abc6e4e3709c715edbae0e7aeb3b1c9f4
turanEbrarr <ebrarkckshn@gmail.com>
2023-05-11 13:13:09 +0300

    turan

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print("turan guncelleme");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hava Durumu',
